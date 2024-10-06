38a35bc89854a9dd16e233d86e48eacb2d8d6c3d
achuasokan <achuasokan555@gmail.com>
2024-10-06 13:42:57 +0530

    Created functions for all discount related codes for adding discounts to products or category based

---
+++ controllers/admin/discountController.js
@@ -0,0 +1,147 @@
+import discountModel from "../../models/discount.js";
+import productModel from "../../models/Product.js"
+import categoryModel from "../../models/Category.js"
+import category from "../../models/Category.js";
+
+
+// //  //  //   //  //          GET   DISCOUNT LIST PAGE   //  //  //  //  //  //  //
+
+export const getDiscountListPage = async (req,res) => {
+  try{
+    const page = parseInt(req.query.page) || 1
+    const limit = 9
+    const skip = (page - 1) * limit
+    
+    const discountList = await discountModel.find({})
+    .populate('product category')
+    .skip(skip).limit(limit)
+
+    const totalDiscounts = await discountModel.countDocuments({})
+    const totalPages = Math.ceil(totalDiscounts / limit)
+    const startIndex = skip + 1;
+
+    res.render('admin/discountList',{
+      discountList,
+      currentPage: page,
+      totalPages,
+      startIndex
+    })
+  } catch(error) {
+    console.log("error in getDiscountListPage", error);
+    res.status(500).send("Internal server error")
+  }
+}
+
+
+// //  //  //   //  //          GET  Add DISCOUNT PAGE   //  //  //  //  //  //  //
+
+export const addDiscountPage = async (req,res) => {
+  try{
+    const message = req.query.message || undefined
+    const productList = await productModel.find({isDeleted: false})
+    const categoryList = await categoryModel.find({isBlocked: false})
+    res.render("admin/addDiscount", {message, productList, categoryList})
+  }catch(error) {
+    console.log("error in addDiscountPage", error);
+    res.status(500).send("Internal server error")
+  }
+}
+
+
+// //  //  //   //  //          POST  Add DISCOUNT   //  //  //  //  //  //  //
+
+export const addDiscount = async (req,res) => {
+  try{
+    
+    const {discountType, discountValue,product,category} =req.body
+    
+    if(discountType === 'product'){
+      const existingDiscount = await discountModel.findOne({product})
+      if(existingDiscount){
+        return res.status(400).redirect(`/admin/addDiscount?message=A discount already exists for this product.`)
+      }
+    }
+
+    if(discountType === 'category'){
+      const existingDiscount = await discountModel.findOne({category})
+      if(existingDiscount){
+        return res.status(400).redirect("/admin/addDiscount?message=A discount already exists for this category.")
+      }
+    }
+
+    const newDiscount = new discountModel({
+      discountType,
+      discountValue,
+      product: discountType === 'product' ? product : null,
+      category: discountType === 'category' ? category : null,
+      
+    })
+
+    await newDiscount.save()
+    res.redirect("/admin/discounts")
+    
+  } catch(error) {
+    console.log("error in addDiscount", error);
+    res.status(500).send("Internal server error")
+  }
+}
+
+
+//  //  //   //  //          GET  Edit DISCOUNT PAGE   //  //  //  //  //  //  //
+
+export const editDiscountPage = async (req,res) => {
+  try{
+    const discountId = req.params.id
+    const discount = await discountModel.findById(discountId)
+    const productList = await productModel.find({isDeleted: false})
+    const categoryList = await categoryModel.find({isBlocked: false})
+    res.render("admin/editDiscount", {discount, productList, categoryList})
+  }catch(error) {
+    console.log("error in editDiscountPage", error);
+    res.status(500).send("Internal server error")
+  }
+}
+
+
+// // //  //  //   //  //          POST  Edit DISCOUNT   //  //  //  //  //  //  //
+
+export const postEditDiscount = async (req,res) => {
+  try{
+    const discountId = req.params.id
+    const {discountType,discountValue,product,category} = req.body
+
+    const updatedDiscount = await discountModel.findByIdAndUpdate(discountId,{
+      discountType,
+      discountValue,
+      product: discountType === 'product' ? product : null,
+      category: discountType === 'category' ? category : null,
+      
+    })
+
+   
+    res.redirect("/admin/discounts")
+
+  }catch(error) {
+    console.log("error in editDiscount", error);
+    res.status(500).send("Internal server error")
+  }
+}
+
+// //  //  //   //  //          POST  Block/Unblock DISCOUNT   //  //  //  //  //  //  //
+
+export const blockDiscount = async (req,res) => {
+  try{
+    const discountId = req.params.id
+    const discount = await discountModel.findById(discountId)
+    if(!discount) {
+      return res.status(404).send("Discount not found")
+    }
+    discount.isActive = !discount.isActive;
+    await discount.save()
+    res.redirect("/admin/discounts")
+
+  }catch(error) {
+    console.log("error in blockDiscount", error);
+    res.status(500).send("Internal server error")
+  }
+}
