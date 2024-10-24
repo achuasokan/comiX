7a63104fcf28d2e3ada2e398ecd7474b9c039a36
achuasokan <achuasokan555@gmail.com>
2024-10-25 00:22:52 +0530

    remove the user from if condition and added return in next()

--- middleware/checkUserSession.js
+++ middleware/checkUserSession.js
@@ -2,14 +2,14 @@ import userModel from "../models/User.js";
 export const checkUserSession = async (req, res, next) => {
 
   try {
-    if (req.session.userID) {
+    if (req.session.userID) { 
       const user = await userModel.findById(req.session.userID);
       if (!user) {
         req.session.destroy()
         req.flash('error','Your account has been blocked. Please contact Support.')
         return res.redirect('/login');
       }
-      if (user && user.isBlocked) {
+      if (user.isBlocked) {
         req.flash('error','Your account has been blocked. Please contact Support.')
         req.session.destroy((error) => {
           if (error) {
@@ -18,17 +18,16 @@ export const checkUserSession = async (req, res, next) => {
           return res.redirect('/login');// Redirect to login page
         });
       } else {
-        
         // Set user details in res.locals
         res.locals.user = req.session.userID;
         res.locals.name = req.session.name;
-        next();
+       return next();
       }
     } else {
       // If not logged in, set user to null
       res.locals.user = null;
       res.locals.name = null;
-      next();
+       return next();
     }
   } catch (error) {
     console.log("Error in check user session", error);
