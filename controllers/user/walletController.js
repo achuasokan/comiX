df89885ad17e0a9ecea8f6d5a05a4a58d0dc819f
achuasokan <achuasokan555@gmail.com>
2024-11-09 04:24:50 +0530

    controllers functions for wallet

---
+++ controllers/user/walletController.js
@@ -0,0 +1,22 @@
+import walletModel from "../../models/wallet.js"
+
+//* //  //  //   //  //          GET WALLET PAGE    //  //  //  //  //  //  //
+
+export const getWalletPage = async (req,res) => {
+  try {
+    const userID = req.session.userID
+
+    const wallet = await walletModel.findOne({user:userID})
+
+    if (!wallet) {
+      return res.status(404).send("wallet not found")
+    }
+    
+    wallet.transaction.sort((a,b)=>b.transactionDate-a.transactionDate)
+
+    res.render('profile/wallet',{wallet})
+  } catch (error) {
+    console.error("error in get wallet page",error);
+    res.status(500).send("internal server error in get wallet page")
+  }
+}
